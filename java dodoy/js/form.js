e2ef90d6898b4f0964275602bb3dc70d5bb8ab03
Godoyzinho <127847388+Godoyzinho@users.noreply.github.com>
2023-10-27 09:05:12 -0300

    Add files via upload

--- java dodoy/js/form.js
+++ java dodoy/js/form.js
@@ -6,51 +6,47 @@ botaoAdicionar.addEventListener("click", function(event) {
 
     var paciente = obtemPacienteDoFormulario(form);
 
-    var pacienteTr = montaTr(paciente);
-
     var erros = validaPaciente(paciente);
 
     if (erros.length > 0) {
         exibeMensagensDeErro(erros);
 
-     return;
-        }
+        return;
+    }
 
-            var tabela = document.querySelector("#tabela-pacientes");
+    adicionaPacienteNaTabela(paciente);
 
-            tabela.appendChild(pacienteTr);
+    form.reset();
 
-            form.reset();
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML = "";
 
-            var mensagensErro = document.querySelector("#mensagens-erro");
-            mensagensErro.innerHTML = "";
-
-        });
+});
 
 function obtemPacienteDoFormulario(form) {
 
-        var paciente = {
-            nome: form.nome.value,
-            peso: form.peso.value,
-            altura: form.altura.value,
-            gordura: form.gordura.value,
-            imc: calculaImc(form.peso.value, form.altura.value)
-        }
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaImc(form.peso.value, form.altura.value)
+    }
 
-        return paciente;
+    return paciente;
 }
 
 function montaTr(paciente) {
-        var pacienteTr = document.createElement("tr");
-        pacienteTr.classList.add("paciente");
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
 
-        pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
-        pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
-        pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
-        pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
-        pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+    pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
 
-        return pacienteTr;
+    return pacienteTr;
 }
 
 function montaTd(dado, classe) {
@@ -101,4 +97,10 @@ function exibeMensagensDeErro(erros) {
         li.textContent = erro;
         ul.appendChild(li);
     });
+}
+
+function adicionaPacienteNaTabela(paciente) {
+    var pacienteTr = montaTr(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
 }
